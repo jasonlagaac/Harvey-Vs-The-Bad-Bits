634e68d30b8c9f422eadd3a0ac79b23afbe76606
Jason Lagaac <jason.lagaac@me.com>
2011-06-26 20:25:03 +0800

    Added parallax scrolling for backgrounds

---
+++ ShadowTypesNew/ParallaxNode.h
@@ -0,0 +1,16 @@
+//
+//  ParallaxNode.h
+//  ShadowTypesNew
+//
+//  Created by neurologik on 24/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ParallaxNode : CCNode {
+    
+}
+
+@end
---
+++ ShadowTypesNew/ParallaxNode.m
@@ -0,0 +1,14 @@
+//
+//  ParallaxNode.m
+//  ShadowTypesNew
+//
+//  Created by neurologik on 24/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ParallaxNode.h"
+
+
+@implementation ParallaxNode
+
+@end

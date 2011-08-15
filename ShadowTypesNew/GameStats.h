06fd68d259b868c023ce96f5894f3e10034090b7
Jason Lagaac <jason.lagaac@me.com>
2011-08-16 00:22:21 +0800

    Added Statistical and Settings classes

---
+++ ShadowTypesNew/GameStats.h
@@ -0,0 +1,13 @@
+//
+//  GameStats.h
+//  ShadowTypesNew
+//
+//  Created by Jason Lagaac on 11/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameStats : NSObject
+
+@end
---
+++ ShadowTypesNew/GameStats.m
@@ -0,0 +1,23 @@
+//
+//  GameStats.m
+//  ShadowTypesNew
+//
+//  Created by Jason Lagaac on 11/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameStats.h"
+
+@implementation GameStats
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
